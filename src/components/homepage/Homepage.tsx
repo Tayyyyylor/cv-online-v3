36daecc134750418eb9dc2b92677642664b8ae60
taylor <bryan.h.pro@gmail.com>
2026-04-26 17:23:26 +0200

    debut du herobanner

--- src/components/homepage/Homepage.tsx
+++ src/components/homepage/Homepage.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import HeroBanner from '../molecules/HeroBanner';
 import { Header } from '../sections/Header'
 
 export default function Homepage() {
   return (
   <>
   <Header />
+  <main>
+    <HeroBanner />
+  </main>
   </>
   )
 }
--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -10,6 +10,7 @@ export default getRequestConfig(async ({ requestLocale }) => {
 
   return {
     locale,
+    timeZone: "Europe/Paris",
     messages: (await import(`../../messages/${locale}.json`)).default,
   };
 });
